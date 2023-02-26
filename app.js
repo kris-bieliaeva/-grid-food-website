cb9234fe3a12e28f3f649c48455f658d7d850c30
Kris <kris.bieliaeva@gmail.com>
2023-02-26 17:34:38 +0300

    smooth scroll

--- app.js
+++ app.js
@@ -12,3 +12,26 @@ mobileMenu.addEventListener("click", function () {
     document.body.style.overflow = "auto";
   }
 });
+
+// Scroll
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+
+// Add a click event listener to each anchor link
+anchorLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    // Prevent default anchor behavior
+    e.preventDefault();
+
+    // Get the target section element
+    const target = document.querySelector(this.getAttribute("href"));
+
+    // Calculate the distance between the top of the target section and the top of the page
+    const offset = target.getBoundingClientRect().top + window.pageYOffset;
+
+    // Scroll to the target section with a smooth animation
+    window.scrollTo({
+      top: offset,
+      behavior: "smooth",
+    });
+  });
+});
